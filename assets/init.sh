8e2ce288968baee94562287917172f4580616250
root <koehlmeier@gmail.com>
2017-07-18 16:16:30 +0200

    postgrey aus default herausgenommen

--- assets/init.sh
+++ assets/init.sh
@@ -104,6 +104,5 @@ fi
 # Actually run Postfix
 newaliases
 rm -f /var/run/rsyslogd.pid
-/usr/sbin/postgrey --pidfile=/var/run/postgrey.pid --daemonize --inet=10023
 /usr/lib/postfix/master &
 rsyslogd -n
